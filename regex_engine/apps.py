81a3268b1d90fc27599151369a93222baab41733
zulfafalah <zulfafalah@samb.co.id>
2025-12-29 11:44:48 +0700

    Add verbose name for Regex Engine app

--- regex_engine/apps.py
+++ regex_engine/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RegexEngineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'regex_engine'
+    verbose_name = 'Regex Engine'

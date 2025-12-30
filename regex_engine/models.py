8a0f34ea91d9d9738077d702b4097c6836fd73cb
zulfafalah <zulfafalah@samb.co.id>
2025-12-30 17:50:24 +0700

    Add choices to customer_name field in CustomerRegexRule model

--- regex_engine/models.py
+++ regex_engine/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class CustomerRegexRule(models.Model):
     customer_id = models.CharField(max_length=100, null=True, blank=True)
-    customer_name = models.CharField(max_length=255)
+    customer_name = models.CharField(max_length=255,
+                                     choices=  (('Food Hall', 'Food Hall'),))
     field_name = models.CharField(max_length=255)
     regex_pattern = models.TextField()
     regex_group = models.IntegerField(null=True, blank=True, default=1)

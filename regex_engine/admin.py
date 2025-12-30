7f1001459abfc1fd4b5f4436b65844f69fdeb962
zulfafalah <zulfafalah@samb.co.id>
2025-12-30 09:45:14 +0700

    Register CustomerRegexRule model with custom admin interface

--- regex_engine/admin.py
+++ regex_engine/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import CustomerRegexRule
 
-admin.site.register(CustomerRegexRule)
+
+@admin.register(CustomerRegexRule)
+class CustomerRegexRuleAdmin(ModelAdmin):
+    list_display = ('customer_name', 'customer_id', 'field_name', 'regex_pattern', 'regex_group')
+    list_filter = ('customer_name', 'field_name')
+    search_fields = ('customer_name', 'customer_id', 'field_name')
+
+    fieldsets = (
+        ('Customer Information', {
+            'fields': ('customer_id', 'customer_name'),
+            'classes': ('tab',)
+        }),
+        ('Regex Configuration', {
+            'fields': ('field_name', 'regex_pattern', 'regex_group'),
+            'classes': ('tab',)
+        }),
+    )

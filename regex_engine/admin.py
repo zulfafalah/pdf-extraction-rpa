6696677f469d91822212ec7935b89b61489bcd94
zulfafalah <zulfafalah@samb.co.id>
2025-12-31 11:09:34 +0700

    Add duplicate rows action to CustomerRegexRule admin interface

--- regex_engine/admin.py
+++ regex_engine/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
 from unfold.admin import ModelAdmin
 from .models import CustomerRegexRule
 
@@ -8,6 +11,7 @@ class CustomerRegexRuleAdmin(ModelAdmin):
     list_display = ('customer_name', 'customer_id', 'field_name', 'regex_pattern', 'regex_group', 'is_item_field')
     list_filter = ('customer_name', 'field_name')
     search_fields = ('customer_name', 'customer_id', 'field_name')
+    actions = ['duplicate_rows']
 
     fieldsets = (
         ('Customer Information', {
@@ -23,3 +27,39 @@ class CustomerRegexRuleAdmin(ModelAdmin):
             'classes': ('tab',)
         }),
     )
+
+    def duplicate_rows(self, request, queryset):
+        """
+        Action for duplicating selected rows
+        """
+        duplicated_count = 0
+        last_duplicated_obj = None
+
+        for obj in queryset:
+            # Save original ID before duplication
+            obj.pk = None
+            obj.id = None
+            # Save new object (duplicate)
+            obj.save()
+            duplicated_count += 1
+            last_duplicated_obj = obj
+
+        # If only 1 row is duplicated, redirect to the edit page
+        if duplicated_count == 1 and last_duplicated_obj:
+            self.message_user(
+                request,
+                f'Row successfully duplicated. You are now editing the duplicate.',
+                messages.SUCCESS
+            )
+            # Redirect to the edit page of the newly duplicated object
+            url = reverse('admin:regex_engine_customerregexrule_change', args=[last_duplicated_obj.pk])
+            return redirect(url)
+
+        # If more than 1 row, show a regular success message
+        self.message_user(
+            request,
+            f'{duplicated_count} rows successfully duplicated.',
+            messages.SUCCESS
+        )
+
+    duplicate_rows.short_description = "Duplicate selected rows"
